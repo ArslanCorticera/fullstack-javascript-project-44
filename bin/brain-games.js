2b307f5c26b936cbc5fb7d57e80d2b624730b58f
ArslanCorticera <arsik.it@yandex.ru>
2025-01-03 00:17:07 +0300

    3 lvl finish

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
-
-import { greetUser } from "../src/cli";
-
+import greetUser from '/Users/arslan/Desktop/fullstack-javascript-project-44/src/cli.js';
 console.log('Welcome to the Brain Games!');
+
 greetUser();
 
