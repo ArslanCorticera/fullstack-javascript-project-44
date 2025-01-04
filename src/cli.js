a412032a21677a3d46fd3500268b500f8053a4b8
ArslanCorticera <arsik.it@yandex.ru>
2025-01-05 01:19:46 +0300

    lk

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from "readline-sync";
 
 function greetUser() {
-  var userName = readlineSync.question("May I have your name? ");
-  console.log("Hello, " + userName + "!");
+  const userName = readlineSync.question("May I have your name? ");
+    console.log(`Hello, ${userName}!`);
 }
 
 export default greetUser;
